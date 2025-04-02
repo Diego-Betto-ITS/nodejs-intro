6507b7e3491b5699e48adec37ef4401f5a19b848
Diego Betto <diego@papion.it>
2025-04-02 16:06:22 +0200

    Rotte base CRUD su server.js

--- src/server.js
+++ src/server.js
@@ -4,25 +4,32 @@ const port = 3000;
 const server = express();
 
 server.get('/timeline', (_req, res) => {
-    res.send([
-        {
-            "id": 1,
-            "year": 2025,
-            "company": "ACME",
-            "role": "Sviluppatore Junior",
-            "description": "Lorem ipsum dolor sit amet, in Lorem duis veniam laborum ipsum nulla proident",
-            "link": "https://google.com"
-        },
-        {
-            "id": 2,
-            "year": 2024,
-            "company": "SIGMA",
-            "role": "Sviluppatore Senior",
-            "description": "Lorem ipsum dolor sit amet, in Lorem duis veniam laborum ipsum nulla proident",
-            "link": "https://google.com"
-        }
-    ]);
+    const timelineContent = require('../data/timeline.json');
+  
+    res.send(timelineContent)
 });
+
+server.get('/timeline/:id', (req, res) => {
+    const timelineContent = require('../data/timeline.json');
+
+    const item = timelineContent.find(item => item.id === parseInt(req.params.id));
+
+    if (!item) {
+        return res.status(404).send();
+    }
+
+    console.log('tutto ok')
+
+    res.send(item);
+});
+
+server.post('/timeline', (req, res) => {
+    res.send('ok POST');
+})
+
+server.put('/timeline/:id', (req, res) => {
+    res.send(`ok PUT con id ${req.params.id}`);
+})
 
 server.listen(port, () => {
     console.log('server in ascolto!')
